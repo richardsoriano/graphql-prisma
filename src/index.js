aa81313f4038a2505082bce02631642539c6ecf1
Richard Soriano <59493601+richardsoriano@users.noreply.github.com>
2021-05-08 09:50:28 -0700

    Init commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import '@babel/polyfill'
+import { GraphQLServer, PubSub } from 'graphql-yoga'
+import db from './db'
+import { resolvers, fragmentReplacements } from './resolvers/index'
+import prisma from './prisma'
+
+const pubsub = new PubSub()
+
+const server = new GraphQLServer({
+    typeDefs: './src/schema.graphql',
+    resolvers,
+    context(request) { 
+        return {
+            db,
+            pubsub,
+            prisma,
+            request
+        }
+    },
+    fragmentReplacements
+})
+
+server.start({ port: process.env.PORT || 4000 },() => {
+    console.log('The server is up!')
+})
+// server.start(() => {
+//     console.log('The server is up!')
+// })
