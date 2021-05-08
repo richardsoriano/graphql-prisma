267accfd34277163896768098a8de3e1ce3e61e5
Richard Soriano <59493601+richardsoriano@users.noreply.github.com>
2021-05-08 10:36:26 -0700

    Extract Prisma secret to env

--- src/utils/getUserId.js
+++ src/utils/getUserId.js
@@ -6,7 +6,7 @@ const getUserId = (request, requestAuth = true) =>{
 
     if (header) {
         const token = header.replace('Bearer ', '')
-        const decoded = jwt.verify(token, 'thisisasecret')
+        const decoded = jwt.verify(token, process.env.PRISMA_AUTH_SECRET)
         return decoded.userId   
     }
 
